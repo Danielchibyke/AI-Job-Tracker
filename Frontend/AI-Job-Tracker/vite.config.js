284fa237e627a14968aca68466895de4c29782a1
Danielchibyke <danielchibyke5@gmail.com>
2025-05-22 23:54:47 +0100

    refactored -

--- Frontend/AI-Job-Tracker/vite.config.js
+++ Frontend/AI-Job-Tracker/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
       '/api': {
         target: 'https://ai-job-tracker-6ekq.onrender.com',
         changeOrigin: true,
-        secure: false,
+        secure: true,
         credentials: true,
        
       },
